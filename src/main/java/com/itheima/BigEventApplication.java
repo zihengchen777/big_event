f84359da30d97dc84613dcf94a5c69a3ea33d0c4
cc <zihengchen1998@outlook.com>
2024-03-26 14:34:30 +0800

    后端环境搭建

---
+++ src/main/java/com/itheima/BigEventApplication.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Chen Ziheng
+ * @Date: 2024/03/26/14:31
+ * @Description:
+ */
+@SpringBootApplication
+public class BigEventApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BigEventApplication.class,args);
+    }
+}
